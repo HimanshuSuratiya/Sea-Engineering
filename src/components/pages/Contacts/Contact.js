2d1eeafd6386df58c544899ea89bec35aa96b0e4
Himanshu Suratiya <Himanshu84688@gmail.com>
2023-02-08 14:20:07 +0530

    Contact page done

--- src/components/pages/Contacts/Contact.js
+++ src/components/pages/Contacts/Contact.js
@@ -3,6 +3,8 @@ import "./Contact.css";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import CallIcon from '@mui/icons-material/Call';
 import EmailIcon from '@mui/icons-material/Email';
+import { TextField, TextareaAutosize } from '@mui/material';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 const Contact = props => {
   return (
@@ -34,6 +36,61 @@ const Contact = props => {
               </div>
               <h4>Mail at</h4>
               <p>Info@xpertis-consulting.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className='col-lg-12 text-center'>
+            <h2 className="font-weight-light mt-3 mb-4">Quick Contact</h2>
+          </div>
+          <div className="col-lg-6">
+            <div className='card-shadow d-flex justify-content-center align-items-center contact-us-img'>
+              <img src="https://storage-asset.msi.com/template/images/contact_us/kv-contact-us-xs.jpg" className="img-fluid" />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="contact-box ml-3">
+              <form className='contact-us-form'>
+                <div className="row">
+                  <div className="col-lg-12">
+                    <TextField
+                      fullWidth
+                      variant='outlined'
+                      size='large'
+                      label={'Name'}
+                    />
+                  </div>
+                  <div className="col-lg-12 mt-2">
+                    <TextField
+                      fullWidth
+                      variant='outlined'
+                      size='large'
+                      label={'Email address'}
+                    />
+                  </div>
+                  <div className="col-lg-12 mt-2">
+                    <TextField
+                      fullWidth
+                      variant='outlined'
+                      type="number"
+                      size='large'
+                      label={'Phone'}
+                    />
+                  </div>
+                  <div className="col-lg-12 mt-2">
+                    <TextareaAutosize
+                      className='p-2'
+                      aria-label="minimum height"
+                      minRows={2}
+                      style={{ width: '100%' }}
+                      placeholder="Message"
+                    />
+                  </div>
+                  <div className="d-flex justify-content-center">
+                    <button className='btn btn-primary btn-lg btn-block contact-us-submit-btn'>Submit <ArrowRightAltIcon /></button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
